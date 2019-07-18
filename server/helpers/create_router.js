45b5f1c4679a7750320a2147c8b8bf47a035eb71
Jordan Davidson <jordandavidson1990@hotmail.com>
2019-07-18 22:35:22 +0100

    mongodb working

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,30 +1,114 @@
 const express = require('express');
+const ObjectID = require('mongodb').ObjectID;
 
-const createRouter = function(data){
+const createRouter = function (collection) {
+
   const router = express.Router();
 
   router.get('/', (req, res) => {
-    res.json(data)
+    collection
+    .find()
+    .toArray()
+    .then((docs) => res.json(docs))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
   });
+
   router.get('/:id', (req, res) => {
-   res.json(data[req.params.id]);
- });
+    const id = req.params.id;
+    collection
+    .findOne({ _id: ObjectID(id) })
+    .then((doc) => res.json(doc))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
 
- router.post('/', (req, res) => {
-   data.push(req.body);
-   res.json(data);
- });
+  router.post('/', (req, res) => {
+    const newData = req.body;
+    collection
+    .insertOne(newData)
+    .then((result) => {
+      res.json(result.ops[0])
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
 
- router.put('/:id', (req, res) => {
-   data[req.params.id] = req.body;
-   res.json(data);
- });
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+    .deleteOne({ _id: ObjectID(id) })
+    .then(result => {
+      res.json(result)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
 
- router.delete('/:id', (req, res) => {
-   data.splice(req.params.id, 1);
-   res.json(data);
- });
+  router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    collection
+    .findOneAndUpdate(
+      { _id: ObjectID(id)},
+      { $set: updatedData },
+      {returnOriginal: false}
+    )
+    .then((result) => {
+      res.json(result.value)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  })
 
   return router;
-}
+
+};
+
 module.exports = createRouter;
+
+// const express = require('express');
+//
+// const createRouter = function(data){
+//   const router = express.Router();
+//
+//   router.get('/', (req, res) => {
+//     res.json(data)
+//   });
+//   router.get('/:id', (req, res) => {
+//    res.json(data[req.params.id]);
+//  });
+//
+//  router.post('/', (req, res) => {
+//    data.push(req.body);
+//    res.json(data);
+//  });
+//
+//  router.put('/:id', (req, res) => {
+//    data[req.params.id] = req.body;
+//    res.json(data);
+//  });
+//
+//  router.delete('/:id', (req, res) => {
+//    data.splice(req.params.id, 1);
+//    res.json(data);
+//  });
+//
+//   return router;
+// }
+// module.exports = createRouter;
--- server/server.js
+++ server/server.js
@@ -3,17 +3,24 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const createRouter = require('./helpers/create_router.js');
+const MongoClient = require('mongodb').MongoClient;
 
-const members = [
-  {name:'Jordan', email:'jordandavidson1990@hotmail.com'},
-  {name:'Gina', email:'gigiloughrey@gmail.com'}
-]
+// const members = [
+//   {name:'Jordan', email:'jordandavidson1990@hotmail.com'},
+//   {name:'Gina', email:'gigiloughrey@gmail.com'}
+// ]
+
 
 app.use(bodyParser.json());
 app.use(cors());
-
-const membersRouter = createRouter(members);
-app.use('/api/members', membersRouter);
+MongoClient.connect('mongodb://localhost:27017')
+.then((client) => {
+  const db = client.db('salt');
+  const members = db.collection('members');
+  const membersRouter = createRouter(members);
+  app.use('/api/members', membersRouter);
+})
+.catch(console.error);
 
 app.listen(3000, function(){
   console.log(`App running on port ${this.address().port}`);
